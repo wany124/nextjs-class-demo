90a8b5d8fa093219f15c28bb9a0a0097e2e42d47
Yiwei Wang <yiwei.wang@vanderbilt.edu>
2024-04-15 11:54:01 -0500

    change welcome message

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Web Dev Project </a>
+          Welcome to <a href="https://nextjs.org">Deployment Lecture </a>
         </h1>
 
         <p className={styles.description}>
